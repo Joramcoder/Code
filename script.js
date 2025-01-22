662e68deeb68472c8e9ced512db25533db8217f7
joram <joramkirubi100@gmail.com>
2025-01-22 22:10:43 +0300

    Add contact form to the website

--- script.js
+++ script.js
@@ -21,3 +21,33 @@ window.addEventListener("scroll", function () {
 function scrollToTop() {
   window.scrollTo({ top: 0, behavior: "smooth" });
 }
+document.querySelector("form").addEventListener("submit", function (event) {
+  const email = document.getElementById("subscriber-email").value;
+  const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+  if (!emailPattern.test(email)) {
+    alert("Please enter a valid email address.");
+    event.preventDefault(); // Prevent form submission
+  }
+});
+// EmailJS configuration (replace with your own EmailJS user ID)
+(function () {
+  emailjs.init("cxVzaflBY3d1p29AI");
+})();
+
+// Contact form submission
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    emailjs.sendForm(" service_4s7619q", "template_unr55tq", this).then(
+      function () {
+        console.log("SUCCESS!");
+        alert("Message sent successfully!");
+      },
+      function (error) {
+        console.log("FAILED...", error);
+        alert("Failed to send message. Please try again.");
+      }
+    );
+  });
